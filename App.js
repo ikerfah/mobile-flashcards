61ba72d5b7e93de48388e41e65161ac366568bf6
KERFAH Issam <i.kerfah@gmail.com>
2021-10-13 21:10:21 +0300

    Setup tab navigation

--- App.js
+++ App.js
@@ -7,21 +7,33 @@ import DecksScreen from "./src/components/DecksScreen";
 import middleware from './src/middleware'
 import DeckDetailsScreen from "./src/components/DeckDetailsScreen";
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { SCREEN_DECKS, SCREEN_DECK_DETAILS_SCREEN } from './src/utils/constants'
+import { SCREEN_DECKS, SCREEN_DECK_DETAILS_SCREEN, SCREEN_NEW_DECK } from './src/utils/constants'
+import AddDeck from "./src/components/AddDeck";
 const store = createStore(reducer, middleware)
+
+const Tab = createBottomTabNavigator();
 
 const Stack = createNativeStackNavigator();
 
+function MainScreen() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen options={{ headerShown: false }} name={SCREEN_DECKS} component={DecksScreen} />
+      <Stack.Screen name={SCREEN_DECK_DETAILS_SCREEN} component={DeckDetailsScreen} />
+    </Stack.Navigator>
+  );
+}
 export default function App() {
   return (
     <NavigationContainer>
       <Provider store={store}>
         <View style={{ flex: 1 }}>
-          <Stack.Navigator>
-            <Stack.Screen name={SCREEN_DECKS} component={DecksScreen} />
-            <Stack.Screen name={SCREEN_DECK_DETAILS_SCREEN} component={DeckDetailsScreen} />
-          </Stack.Navigator>
+          <Tab.Navigator screenOptions={{ tabBarIconStyle: { display: "none" } }}>
+            <Tab.Screen options={{ headerShown: false }} name="DECKS" component={MainScreen} i />
+            <Tab.Screen options={{ headerShown: false }} name={SCREEN_NEW_DECK} component={AddDeck} />
+          </Tab.Navigator>
         </View>
       </Provider>
     </NavigationContainer>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
 export const SCREEN_DECKS = "DecksScreen"
 export const SCREEN_DECK_DETAILS_SCREEN = "DeckDetailsScreen"
+export const SCREEN_NEW_DECK = "NEW DECK"
