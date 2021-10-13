31178845bbb1a783ae50f43d6504e3ef23379b42
KERFAH Issam <i.kerfah@gmail.com>
2021-10-13 19:44:20 +0300

    Setup stack navigation

--- App.js
+++ App.js
@@ -5,15 +5,25 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import MainScreen from "./src/components/MainScreen";
 import middleware from './src/middleware'
+import DeckDetailsScreen from "./src/components/DeckDetailsScreen";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const store = createStore(reducer, middleware)
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
-    <Provider store={store}>
-      <View style={{ flex: 1 }}>
-        <MainScreen />
-      </View>
-    </Provider>
+    <NavigationContainer>
+      <Provider store={store}>
+        <View style={{ flex: 1 }}>
+          <Stack.Navigator>
+            <Stack.Screen name="MainScreen" component={MainScreen} />
+            <Stack.Screen name="DeckDetailsScreen" component={DeckDetailsScreen} />
+          </Stack.Navigator>
+        </View>
+      </Provider>
+    </NavigationContainer>
   )
 }
